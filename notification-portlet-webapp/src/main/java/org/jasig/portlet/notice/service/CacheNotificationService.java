50220e26636eb5f3b1f85c270c4bcb01e172807f
Josh Helmer <jhelmer@unicon.net>
2015-03-03 16:19:35 -0700

    Remove debug code.

--- notification-portlet-webapp/src/main/java/org/jasig/portlet/notice/service/CacheNotificationService.java
+++ notification-portlet-webapp/src/main/java/org/jasig/portlet/notice/service/CacheNotificationService.java
@@ -107,8 +107,7 @@ public final class CacheNotificationService extends AbstractNotificationService 
 
         NotificationResponse rslt = new NotificationResponse();
         final String cacheKey = createServiceUserWindowSpecificCacheKey(req);
-//        final Element m = cache.get(cacheKey);
-        final Element m = null;
+        final Element m = cache.get(cacheKey);
         if (m != null) {
             if (log.isDebugEnabled()) {
                 log.debug("Cache HIT for user='" + username 
