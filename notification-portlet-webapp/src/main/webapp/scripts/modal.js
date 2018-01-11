9f8dbf58205ccaf2b7332ff9fb38406253994648
Brandon Powell <bpowell29a@gmail.com>
2018-01-11 13:46:31 -0500

    Switched to let and const
    Dropped the YUI Compressor for minify maven plugin. YUI Compressor was
    failing on const/let in javascript

--- notification-portlet-webapp/src/main/webapp/scripts/modal.js
+++ notification-portlet-webapp/src/main/webapp/scripts/modal.js
@@ -18,15 +18,15 @@ new Vue({
     gotoNext: function(index) {
       this.items[index].show = false
 
-      var modal = document.getElementById(this.id)
-      var doc = modal && modal.ownerDocument
+      const modal = document.getElementById(this.id)
+      let doc = modal && modal.ownerDocument
       doc.removeEventListener("focus", this.focusListener, true)
       doc.removeEventListener("keyup", this.keyUpListener)
 
       if (this.items[index + 1] !== undefined) {
         this.items[index + 1].show = true
         this.id = "notification" + index + 2
-        var that = this
+        let that = this
         setTimeout(function() {
           that.ada(index + 1)
         }, 250)
@@ -35,19 +35,19 @@ new Vue({
           this.lastActive.focus()
         }
 
-        var body = document.getElementsByTagName("body")[0]
-        for (x = 0; x < body.childElementCount; x++) {
+        let body = document.getElementsByTagName("body")[0]
+        for (let x = 0; x < body.childElementCount; x++) {
           body.children[x].removeAttribute("aria-hidden")
         }
         body.style.overflow = this.bodyOverflow
       }
     },
     submit: function(notificationId, actionId, index) {
-      var formData = {
+      const formData = {
         notificationId: notificationId,
         actionId: actionId
       }
-      var formBody = Object.keys(formData)
+      const formBody = Object.keys(formData)
         .map(function(key) {
           return (
             encodeURIComponent(key) + "=" + encodeURIComponent(formData[key])
@@ -69,8 +69,8 @@ new Vue({
         )
     },
     ada: function(index) {
-      var modal = document.getElementById(this.id)
-      var doc = modal && modal.ownerDocument
+      const modal = document.getElementById(this.id)
+      let doc = modal && modal.ownerDocument
       if (doc !== null && doc !== undefined) {
         doc.index = index
         doc.addEventListener("focus", this.focusListener, true)
@@ -78,11 +78,11 @@ new Vue({
       }
     },
     focusListener: function() {
-      var modal = document.getElementById(this.id)
-      var doc = modal && modal.ownerDocument
+      const modal = document.getElementById(this.id)
+      const doc = modal && modal.ownerDocument
 
-      var active = doc.activeElement
-      var content = modal && modal.lastChild
+      const active = doc.activeElement
+      const content = modal && modal.lastChild
 
       if (content && content !== active && !content.contains(active)) {
         content.focus()
@@ -96,8 +96,8 @@ new Vue({
   },
   mounted: function() {
     this.$nextTick(function() {
-      var that = this
-      var body = document.getElementsByTagName("body")[0]
+      let that = this
+      let body = document.getElementsByTagName("body")[0]
 
       this.lastActive = document.activeElement
       this.bodyOverflow = body.style.overflow
@@ -105,8 +105,8 @@ new Vue({
       this.$http.post(invokeNotificationServiceUrl)
       this.$http.post(getNotifications).then(function(response) {
         this.items.pop()
-        for (i = 0; i < response.data.feed.length; i++) {
-          var item = response.data.feed[i]
+        for (let i = 0; i < response.data.feed.length; i++) {
+          let item = response.data.feed[i]
           item.show = false
           this.items.push(item)
         }
@@ -115,7 +115,7 @@ new Vue({
           this.items[0].show = true
           setTimeout(function() {
             that.ada(0)
-            for (x = 0; x < body.childElementCount; x++) {
+            for (let x = 0; x < body.childElementCount; x++) {
               body.children[x].setAttribute("aria-hidden", "true")
             }
             body.style.overflow = "hidden"
