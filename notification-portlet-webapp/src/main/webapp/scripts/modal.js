db88e8e781fc6f4444bc9f43c002b7d7eb4f3804
Christian Murphy <cmurphy@unicon.net>
2018-03-21 13:21:46 -0700

    refactor: add catches for promises, use named key instead of keyCode

--- notification-portlet-webapp/src/main/webapp/scripts/modal.js
+++ notification-portlet-webapp/src/main/webapp/scripts/modal.js
@@ -3,7 +3,7 @@ Vue.component("modal", {
   props: {
     id: String
   }
-})
+});
 
 // start app
 new Vue({
@@ -16,113 +16,114 @@ new Vue({
   },
   methods: {
     gotoNext: function(index) {
-      this.items[index].show = false
+      this.items[index].show = false;
 
-      const modal = document.getElementById(this.id)
-      let doc = modal && modal.ownerDocument
-      doc.removeEventListener("focus", this.focusListener, true)
-      doc.removeEventListener("keyup", this.keyUpListener)
+      const modal = document.getElementById(this.id);
+      let doc = modal && modal.ownerDocument;
+      doc.removeEventListener("focus", this.focusListener, true);
+      doc.removeEventListener("keyup", this.keyUpListener);
 
       if (this.items[index + 1] !== undefined) {
-        this.items[index + 1].show = true
-        this.id = "notification" + index + 2
-        let that = this
+        this.items[index + 1].show = true;
+        this.id = "notification" + index + 2;
+        let that = this;
         setTimeout(function() {
-          that.attachListeners(index + 1)
-        }, 250)
+          that.attachListeners(index + 1);
+        }, 250);
       } else {
         if (this.lastActive) {
-          this.lastActive.focus()
+          this.lastActive.focus();
         }
 
-        let body = document.getElementsByTagName("body")[0]
+        let body = document.getElementsByTagName("body")[0];
         for (let x = 0; x < body.childElementCount; x++) {
-          body.children[x].removeAttribute("aria-hidden")
+          body.children[x].removeAttribute("aria-hidden");
         }
-        body.style.overflow = this.bodyOverflow
+        body.style.overflow = this.bodyOverflow;
       }
     },
     submit: function(notificationId, actionId, index) {
       const formData = {
         notificationId: notificationId,
         actionId: actionId
-      }
+      };
       const formBody = Object.keys(formData)
-        .map(function(key) {
-          return (
+        .map(
+          key =>
             encodeURIComponent(key) + "=" + encodeURIComponent(formData[key])
-          )
-        })
-        .join("&")
+        )
+        .join("&");
       this.$http
         .post(invokeActionUrl, formBody, {
           credentials: "include",
           headers: { "Content-Type": "application/x-www-form-urlencoded" }
         })
-        .then(
-          function(response) {
-            this.gotoNext(index)
-          },
-          function(response) {
-            console.log("Problem posting request")
-          }
-        )
+        .then(function(response) {
+          this.gotoNext(index);
+        })
+        .catch(function(response) {
+          console.log("Problem posting request");
+        });
     },
     attachListeners: function(index) {
-      const modal = document.getElementById(this.id)
-      let doc = modal && modal.ownerDocument
+      const modal = document.getElementById(this.id);
+      let doc = modal && modal.ownerDocument;
       if (doc !== null && doc !== undefined) {
-        doc.index = index
-        doc.addEventListener("focus", this.focusListener, true)
-        doc.addEventListener("keyup", this.keyUpListener)
+        doc.index = index;
+        doc.addEventListener("focus", this.focusListener, true);
+        doc.addEventListener("keyup", this.keyUpListener);
       }
     },
     focusListener: function() {
-      const modal = document.getElementById(this.id)
-      const doc = modal && modal.ownerDocument
+      const modal = document.getElementById(this.id);
+      const doc = modal && modal.ownerDocument;
 
-      const active = doc.activeElement
-      const content = modal && modal.lastChild
+      const active = doc.activeElement;
+      const content = modal && modal.lastChild;
 
       if (content && content !== active && !content.contains(active)) {
-        content.focus()
+        content.focus();
       }
     },
     keyUpListener: function(e) {
-      //27 is the ESCAPE key keyCode
-      if (!e.keyCode || e.keyCode === 27) {
-        this.gotoNext(e.currentTarget.index)
+      if (!e.key || e.key === "Escape") {
+        this.gotoNext(e.currentTarget.index);
       }
     }
   },
   mounted: function() {
     this.$nextTick(function() {
-      let that = this
-      let body = document.getElementsByTagName("body")[0]
+      let that = this;
+      let body = document.getElementsByTagName("body")[0];
 
-      this.lastActive = document.activeElement
-      this.bodyOverflow = body.style.overflow
+      this.lastActive = document.activeElement;
+      this.bodyOverflow = body.style.overflow;
 
-      this.$http.post(invokeNotificationServiceUrl)
-      this.$http.post(getNotifications).then(function(response) {
-        this.items.pop()
-        this.items = response.data.feed.map(function(item) {
-          item.show = false
-          return item
+      this.$http
+        .post(invokeNotificationServiceUrl)
+        .catch(err => console.error(err));
+      this.$http
+        .post(getNotifications)
+        .then(function(response) {
+          this.items.pop();
+          this.items = response.data.feed.map(function(item) {
+            item.show = false;
+            return item;
+          });
+          if (this.items.length >= 1) {
+            this.id = "notification1";
+            this.items[0].show = true;
+            setTimeout(function() {
+              that.attachListeners(0);
+              for (let x = 0; x < body.childElementCount; x++) {
+                body.children[x].setAttribute("aria-hidden", "true");
+              }
+              body.style.overflow = "hidden";
+              document.getElementById("notification1").focus();
+            }, 250);
+          }
         })
-        if (this.items.length >= 1) {
-          this.id = "notification1"
-          this.items[0].show = true
-          setTimeout(function() {
-            that.attachListeners(0)
-            for (let x = 0; x < body.childElementCount; x++) {
-              body.children[x].setAttribute("aria-hidden", "true")
-            }
-            body.style.overflow = "hidden"
-            document.getElementById("notification1").focus()
-          }, 250)
-        }
-      })
-    })
+        .catch(err => console.error(err));
+    });
   }
-})
+});
