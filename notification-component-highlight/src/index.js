b8de578b57975d83b72da313c11dde48075372d7
Christian Murphy <cmurphy@unicon.net>
2018-03-23 14:27:42 -0700

    feat: include component on pages as a web component

--- notification-component-highlight/src/index.js
+++ notification-component-highlight/src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'reactive-elements';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+document.registerReact('notification-icon', App);
