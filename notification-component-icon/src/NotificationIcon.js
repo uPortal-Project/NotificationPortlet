282cb5b29fb29279dc38cdd42238ac691fcb02e3
Christian Murphy <cmurphy@unicon.net>
2018-03-24 16:38:22 -0700

    refactor: combine render empty and one or many into render notifications

--- notification-component-icon/src/NotificationIcon.js
+++ notification-component-icon/src/NotificationIcon.js
@@ -34,56 +34,51 @@ class NotificationIcon extends Component {
     });
   };
 
-  renderEmpty = () => (
-    <DropdownItem className="up-notification--menu-item" disabled>
-      congrats no notifications
-    </DropdownItem>
-  );
-
-  renderOneOrMany = () =>
-    this.state.notifications.map(({url, message, isRead}) => (
-      <DropdownItem
-        tag="a"
-        className={'up-notification--menu-item ' + (isRead ? 'read' : 'unread')}
-        href={url}
-      >
-        {message}
+  renderNotifications = () =>
+    this.state.notifications.length < 1 ? (
+      <DropdownItem className="up-notification--menu-item" disabled>
+        congrats no notifications
       </DropdownItem>
-    ));
-
-  render() {
-    return (
-      <Dropdown
-        isOpen={this.state.dropdownOpen}
-        toggle={this.toggle}
-        className="up-notification"
-      >
-        <DropdownToggle
-          onClick={this.toggle}
-          className="up-notification--toggle"
+    ) : (
+      this.state.notifications.map(({url, message, isRead}) => (
+        <DropdownItem
+          tag="a"
+          className={
+            'up-notification--menu-item ' + (isRead ? 'read' : 'unread')
+          }
+          href={url}
         >
-          <span className="sr-only">notifications</span>
-          <FontAwesomeIcon icon="bell" />
-        </DropdownToggle>
-        <DropdownMenu className="up-notification--menu">
-          <DropdownItem className="up-notification--menu-header" header>
-            Notifications
-          </DropdownItem>
-          {this.state.notifications.length < 1
-            ? this.renderEmpty()
-            : this.renderOneOrMany()}
-          <DropdownItem
-            className="up-notification--menu-footer"
-            tag="a"
-            href="/p/notifications"
-            header
-          >
-            see all notifications
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
+          {message}
+        </DropdownItem>
+      ))
     );
-  }
+
+  render = () => (
+    <Dropdown
+      isOpen={this.state.dropdownOpen}
+      toggle={this.toggle}
+      className="up-notification"
+    >
+      <DropdownToggle onClick={this.toggle} className="up-notification--toggle">
+        <span className="sr-only">notifications</span>
+        <FontAwesomeIcon icon="bell" />
+      </DropdownToggle>
+      <DropdownMenu className="up-notification--menu">
+        <DropdownItem className="up-notification--menu-header" header>
+          Notifications
+        </DropdownItem>
+        {this.renderNotifications()}
+        <DropdownItem
+          className="up-notification--menu-footer"
+          tag="a"
+          href="/p/notifications"
+          header
+        >
+          see all notifications
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
 }
 
 export default NotificationIcon;
