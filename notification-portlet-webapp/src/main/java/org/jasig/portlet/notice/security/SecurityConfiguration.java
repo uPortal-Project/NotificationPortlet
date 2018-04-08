8bf165a3b74ea7c68f136ec3b5a21787b96d8473
Andrew Wills <wills.drew@gmail.com>
2018-04-08 12:22:00 -0700

    Issue #71: Troubleshooting the server-side Spring Security configs;  NOTE!  uPortalVersion=5.1.0-SNAPSHOT (WIP)

--- notification-portlet-webapp/src/main/java/org/jasig/portlet/notice/security/SecurityConfiguration.java
+++ notification-portlet-webapp/src/main/java/org/jasig/portlet/notice/security/SecurityConfiguration.java
@@ -20,17 +20,28 @@ package org.jasig.portlet.notice.security;
 
 import org.apereo.portal.soffit.security.SoffitApiAuthenticationManager;
 import org.apereo.portal.soffit.security.SoffitApiPreAuthenticatedProcessingFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.ErrorPageFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static org.apereo.portal.soffit.service.AbstractJwtService.DEFAULT_SIGNATURE_KEY;
+import static org.apereo.portal.soffit.service.AbstractJwtService.SIGNATURE_KEY_PROPERTY;
 
 /**
- * Configuration of Spring Security for REST APIs.
+ * Configuration of Spring Security for REST APIs.  (Portlet requests are not covered by these
+ * settings.)
  *
  * @since 4.0
  */
@@ -38,10 +49,33 @@ import org.springframework.security.web.authentication.preauth.AbstractPreAuthen
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${" + SIGNATURE_KEY_PROPERTY + ":" + DEFAULT_SIGNATURE_KEY + "}")
+    private String signatureKey;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        /*
+         * Since this module includes portlets, we only want to apply Spring Security to requests
+         * targeting out REST APIs.
+         */
+        final RequestMatcher pathMatcher = new AntPathRequestMatcher("/api/**");
+        final RequestMatcher inverseMatcher = new NegatedRequestMatcher(pathMatcher);
+        web.ignoring().requestMatchers(inverseMatcher);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        /*
+         * Provide a SoffitApiPreAuthenticatedProcessingFilter (from uPortal) that is NOT a
+         * top-level bean in the Spring Application Context.
+         */
+        final AbstractPreAuthenticatedProcessingFilter filter =
+                new SoffitApiPreAuthenticatedProcessingFilter(signatureKey);
+        filter.setAuthenticationManager(authenticationManager());
+
         http
-            .addFilter(preAuthenticatedProcessingFilter())
+            .addFilter(filter)
             .authorizeRequests()
                 .antMatchers(HttpMethod.GET,"/api/v1/**").access("hasRole('REST_READ') or hasRole('REST_WRITE')")
                 .antMatchers(HttpMethod.POST,"/api/v1/**").hasRole("REST_WRITE")
@@ -51,7 +85,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/api/v2/**").authenticated()
                 .antMatchers(HttpMethod.DELETE,"/api/v2/**").denyAll()
                 .antMatchers(HttpMethod.PUT,"/api/v2/**").denyAll()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+            .and()
+            /*
+             * Session fixation protection is provided by uPortal.  Since portlet tech requires
+             * sessionCookiePath=/, we will make the portal unusable if other modules are changing
+             * the sessionId as well.
+             */
+            .sessionManagement()
+                .sessionFixation().none();
+
     }
 
     @Bean
@@ -60,10 +103,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public AbstractPreAuthenticatedProcessingFilter preAuthenticatedProcessingFilter() {
-        final AbstractPreAuthenticatedProcessingFilter rslt = new SoffitApiPreAuthenticatedProcessingFilter();
-        rslt.setAuthenticationManager(authenticationManager());
-        return rslt;
+    public ErrorPageFilter errorPageFilter() {
+        return new ErrorPageFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean disableSpringBootErrorFilter() {
+        /*
+         * The ErrorPageFilter (Spring) makes extra calls to HttpServletResponse.flushBuffer(),
+         * and this behavior produces many warnings in the portal logs during portlet requests.
+         */
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(errorPageFilter());
+        filterRegistrationBean.setEnabled(false);
+        return filterRegistrationBean;
     }
 
 }
