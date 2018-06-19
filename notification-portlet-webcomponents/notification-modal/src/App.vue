84c829a2f705032618ae6e158c6e1bb769f843db
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-06-19 15:41:08 -0700

    refactor: move templates into vue file

--- notification-portlet-webcomponents/notification-modal/src/App.vue
+++ notification-portlet-webcomponents/notification-modal/src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NotificationModal />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NotificationModal from "./components/Modal.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    NotificationModal
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ notification-portlet-webcomponents/notification-modal/src/components/Modal/Modal.js
@@ -0,0 +1,132 @@
+export default {
+  props: {
+    id: String,
+    invokeActionUrl: String,
+    invokeNotificationServiceUrl: String,
+    getNotifications: String
+  },
+  data: {
+    items: [{ show: false }],
+    id: "",
+    lastActive: null,
+    bodyOverflow: null
+  },
+  methods: {
+    gotoNext: function(index) {
+      this.items[index].show = false;
+
+      const modal = document.getElementById(this.id);
+      let doc = modal && modal.ownerDocument;
+      doc.removeEventListener("focus", this.focusListener, true);
+      doc.removeEventListener("keyup", this.keyUpListener);
+
+      if (this.items[index + 1] !== undefined) {
+        this.items[index + 1].show = true;
+        this.id = "notification" + index + 2;
+        let that = this;
+        setTimeout(function() {
+          that.attachListeners(index + 1);
+        }, 250);
+      } else {
+        if (this.lastActive) {
+          this.lastActive.focus();
+        }
+
+        let body = document.getElementsByTagName("body")[0];
+        for (let x = 0; x < body.childElementCount; x++) {
+          body.children[x].removeAttribute("aria-hidden");
+        }
+        body.style.overflow = this.bodyOverflow;
+      }
+    },
+    submit: function(notificationId, actionId, index) {
+      const formData = {
+        notificationId: notificationId,
+        actionId: actionId
+      };
+      const formBody = Object.keys(formData)
+        .map(function(key) {
+          return (
+            encodeURIComponent(key) + "=" + encodeURIComponent(formData[key])
+          );
+        })
+        .join("&");
+      this.$http
+        .post(this.invokeActionUrl, formBody, {
+          credentials: "include",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        })
+        .then(function() {
+          this.gotoNext(index);
+        })
+        .catch(function() {
+          // TODO: add an error view
+          // eslint-disable-next-line
+          console.error("Problem posting request");
+        });
+    },
+    attachListeners: function(index) {
+      const modal = document.getElementById(this.id);
+      let doc = modal && modal.ownerDocument;
+      if (doc !== null && doc !== undefined) {
+        doc.index = index;
+        doc.addEventListener("focus", this.focusListener, true);
+        doc.addEventListener("keyup", this.keyUpListener);
+      }
+    },
+    focusListener: function() {
+      const modal = document.getElementById(this.id);
+      const doc = modal && modal.ownerDocument;
+
+      const active = doc.activeElement;
+      const content = modal && modal.lastChild;
+
+      if (content && content !== active && !content.contains(active)) {
+        content.focus();
+      }
+    },
+    keyUpListener: function(e) {
+      //27 is the ESCAPE key keyCode
+      if (!e.keyCode || e.keyCode === 27) {
+        this.gotoNext(e.currentTarget.index);
+      }
+    }
+  },
+  mounted: function() {
+    this.$nextTick(function() {
+      let that = this;
+      let body = document.getElementsByTagName("body")[0];
+
+      this.lastActive = document.activeElement;
+      this.bodyOverflow = body.style.overflow;
+
+      this.$http.post(this.invokeNotificationServiceUrl);
+      this.$http
+        .post(this.getNotifications)
+        .then(function(response) {
+          this.items.pop();
+          this.items = response.data.feed.map(function(item) {
+            item.show = false;
+            return item;
+          });
+          if (this.items.length >= 1) {
+            this.id = "notification1";
+            this.items[0].show = true;
+            setTimeout(function() {
+              that.attachListeners(0);
+              for (let x = 0; x < body.childElementCount; x++) {
+                body.children[x].setAttribute("aria-hidden", "true");
+              }
+              body.style.overflow = "hidden";
+              document.getElementById("notification1").focus();
+            }, 250);
+          }
+        })
+        .catch(function(err) {
+          // TODO: add an error view
+          // eslint-disable-next-line
+          console.error(err);
+        });
+    });
+  }
+};
