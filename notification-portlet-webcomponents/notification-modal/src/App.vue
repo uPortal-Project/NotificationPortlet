f3a0c142b00d102c6ae267887b8b9927af0b3fdf
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-07-10 14:24:48 -0700

    feat(notification-modal): use bootstrap-vue to provide accessible modal

--- notification-portlet-webcomponents/notification-modal/src/App.vue
+++ notification-portlet-webcomponents/notification-modal/src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <NotificationModal msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NotificationModal from "./components/NotificationModal.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    NotificationModal
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ notification-portlet-webcomponents/notification-modal/src/components/NotificationModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-modal v-model="modalShow">
+    <p>Hello from modal!</p>
+  </b-modal>
+</template>
+
+<script>
+import bModal from "bootstrap-vue/es/components/modal/modal";
+
+export default {
+  name: "NotificationModal",
+  props: {
+    msg: String
+  },
+  components: {
+    "b-modal": bModal
+  },
+  data() {
+    return {
+      modalShow: true
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+// core bootstrap framework
+@import "../../node_modules/bootstrap/scss/functions.scss";
+@import "../../node_modules/bootstrap/scss/variables.scss";
+@import "../../node_modules/bootstrap/scss/mixins.scss";
+// bootstrap styles needed by page
+@import "../../node_modules/bootstrap/scss/utilities.scss";
+@import "../../node_modules/bootstrap/scss/type.scss";
+@import "../../node_modules/bootstrap/scss/buttons.scss";
+@import "../../node_modules/bootstrap/scss/close.scss";
+@import "../../node_modules/bootstrap/scss/modal.scss";
+</style>
