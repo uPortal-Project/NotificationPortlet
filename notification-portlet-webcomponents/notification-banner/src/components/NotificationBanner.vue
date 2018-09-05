cba2b6b00c9ef2c3145f799f37ff76ff8c60c955
Drew Wills <wills.drew@gmail.com>
2018-09-05 10:37:45 -0700

    Issue #99:  add some JavaScript fundamental to a Notification display strategy borrowed from notification-modal

--- notification-portlet-webcomponents/notification-banner/src/components/NotificationBanner.vue
+++ notification-portlet-webcomponents/notification-banner/src/components/NotificationBanner.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="notification-banner">
+    <b-alert varient="info" show>
+      Hello World!
+    </b-alert>
   </div>
 </template>
 
 <script>
+import bAlert from "bootstrap-vue/es/components/alert/alert";
+import oidc from "@uportal/open-id-connect/esm/open-id-connect";
+
 export default {
   name: 'NotificationBanner',
+
   props: {
-    msg: String
-  }
+    userInfoApiUrl: {
+      type: String,
+      default: "/uPortal/api/v5-1/userinfo"
+    },
+    notificationApiUrl: {
+      type: String,
+      default: "/NotificationPortlet/api/v2/notifications"
+    }
+  },
+
+  components: {
+    "b-alert": bAlert
+  },
+
+  data() {
+    return {
+      // list of notifications to display
+      notifications: []
+    };
+  },
+
+  methods: {
+    async fetchNotifications() {
+      // read props
+      const { notificationApiUrl, userInfoApiUrl } = this;
+
+      try {
+        // get user token, skipped in debug mode
+        const { encoded: token } = await oidc({ userInfoApiUrl });
+
+        // gather notifications
+        const { data: notifications } = await get(notificationApiUrl, {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "content-type": "application/jwt"
+          }
+        });
+
+        // store notifications to state
+        this.notifications = notifications;
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
+    }
+  },
+
+  // entrypoint
+  created() {
+    return this.fetchNotifications();
+  },
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
