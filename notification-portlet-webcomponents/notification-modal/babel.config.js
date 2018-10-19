23a9bdc1bf3f292a0b54e8417cd6804b97621d82
Drew Wills <wills.drew@gmail.com>
2018-10-18 18:10:46 -0700

    fix:  revert @vue/babel-preset-app -> @babel/preset-env

--- notification-portlet-webcomponents/notification-modal/babel.config.js
+++ notification-portlet-webcomponents/notification-modal/babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache.never();
   return {
     plugins: ["babel-plugin-transform-custom-element-classes"],
-    presets: [["@vue/babel-preset-app", { useBuiltIns: false }]]
+    presets: [["@babel/preset-env", { useBuiltIns: 'usage' }]]
   };
 };
