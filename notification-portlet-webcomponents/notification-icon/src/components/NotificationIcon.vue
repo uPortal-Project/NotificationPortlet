3ebfb9a2a3305907e164df8399304099535f916d
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-11-28 11:41:00 -0700

    refactor: replace notification-component-icon, with notification-icon
    Replaces React with Vue.
    Supports scoped styles to avoid style bleed out.
    
    resolves #125

---
+++ notification-portlet-webcomponents/notification-icon/src/components/NotificationIcon.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="notification-icon">
+    <dropdown id="_uid" no-caret :variant="variant">
+      <template slot="button-content">
+        <font-awesome-icon icon="bell"/>
+        {{ unreadCount }}
+      </template>
+      <dropdown-header>Notifications</dropdown-header>
+      <notification-item
+        v-for="notification in notifications"
+        :key="notification.id || notification.body"
+        :notification="notification"
+      ></notification-item>
+      <dropdown-item :href="seeAllNotificationsUrl">See All Notifications</dropdown-item>
+    </dropdown>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import AsyncComputed from "vue-async-computed";
+import Dropdown from "bootstrap-vue/es/components/dropdown/dropdown";
+import DropdownHeader from "bootstrap-vue/es/components/dropdown/dropdown-header";
+import DropdownItem from "bootstrap-vue/es/components/dropdown/dropdown-item";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBell } from "@fortawesome/free-solid-svg-icons/faBell";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import oidc from "@uportal/open-id-connect";
+import ky from "ky";
+import NotificationItem from "./NotificationItem";
+
+Vue.use(AsyncComputed);
+
+library.add(faBell);
+
+export default {
+  name: "NotificationIcon",
+  props: {
+    userInfoApiUrl: {
+      type: String,
+      default: "/uPortal/api/v5-1/userinfo"
+    },
+    notificationApiUrl: {
+      type: String,
+      default: "/NotificationPortlet/api/v2/notifications"
+    },
+    seeAllNotificationsUrl: {
+      type: String,
+      default: "/uPortal/p/notification"
+    },
+    debug: {
+      type: Boolean,
+      default: false
+    }
+  },
+  asyncComputed: {
+    notifications: {
+      async get() {
+        const { notificationApiUrl, debug } = this;
+
+        try {
+          const headers = debug
+            ? {}
+            : {
+                Authorization: "Bearer " + (await oidc()).encoded,
+                "content-type": "application/jwt"
+              };
+          return await ky.get(notificationApiUrl, { headers }).json();
+        } catch (err) {
+          console.error(err);
+          return [];
+        }
+      },
+      default: [],
+      lazy: true
+    }
+  },
+  computed: {
+    unreadCount() {
+      return this.notifications.filter(
+        ({ attributes }) => !JSON.parse(attributes?.READ?.[0] || "true")
+      ).length;
+    },
+    variant() {
+      return this.unreadCount < 1 ? "default" : "danger";
+    }
+  },
+  components: {
+    Dropdown,
+    DropdownHeader,
+    DropdownItem,
+    FontAwesomeIcon,
+    NotificationItem
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.notification-icon /deep/ {
+  // core bootstrap framework
+  @import "../../node_modules/bootstrap/scss/functions";
+  @import "../../node_modules/bootstrap/scss/variables";
+  @import "../../node_modules/bootstrap/scss/mixins";
+  // bootstrap styles needed by component
+  @import "../../node_modules/bootstrap/scss/reboot";
+  @import "../../node_modules/bootstrap/scss/dropdown";
+  @import "../../node_modules/bootstrap/scss/buttons";
+}
+</style>
---
+++ notification-portlet-webcomponents/notification-icon/src/components/NotificationItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <dropdown-item :target="target" :href="url" @click="onClickAction()">{{notification.body}}</dropdown-item>
+</template>
+
+<script>
+import DropdownItem from "bootstrap-vue/es/components/dropdown/dropdown-item";
+
+export default {
+  name: "NotificationItem",
+  props: {
+    notification: Object
+  },
+  computed: {
+    redirectAction() {
+      return this.notification?.availableActions?.some?.(
+        ({ id }) => id === "MarkAsReadAndRedirectAction"
+      );
+    },
+    isRead() {
+      return JSON.parse(this.notification?.attributes?.READ?.[0] || "true");
+    },
+    url() {
+      return this.redirectAction ? "javascript:void(0)" : this.notification.url;
+    },
+    onClickAction() {
+      if (this.redirectAction) {
+        return () => {
+          let form = document.createElement("form");
+          form.action = this.notification.url;
+          form.method = "POST";
+          form.style.display = "none";
+          document.body.appendChild(form);
+          form.submit();
+        };
+      }
+      return () => {};
+    },
+    target() {
+      return this.redirectAction ? "_self" : "_blank";
+    }
+  },
+  components: {
+    DropdownItem
+  }
+};
+</script>
