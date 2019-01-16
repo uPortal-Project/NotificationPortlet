ba209c27b0904c547594982a3364306014a53f46
Jeff Sittler <mindblender@gmail.com>
2019-01-16 15:50:58 -0700

    Wrapping the slot to render the non-item items

--- notification-portlet-webcomponents/notification-icon/src/components/ieDropdownItem.vue
+++ notification-portlet-webcomponents/notification-icon/src/components/ieDropdownItem.vue
@@ -1,9 +1,13 @@
 <template>
-    <slot></slot>
+    <a v-if="href" class="dropdown-item" :href="href"><slot></slot></a>
+    <div v-else class="dropdown-item">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "ie-dropdown-item"
+  name: "ie-dropdown-item",
+  props: ['href'],
 };
 </script>
